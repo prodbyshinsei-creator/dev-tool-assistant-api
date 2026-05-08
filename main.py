eb97d345f9bdd9ed5240719399956ecc2ac073fb
prodbyshinsei-creator <prod.by.shinsei@gmail.com>
2026-05-08 16:57:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from vamp_engine import get_token_metadata
 
 app = FastAPI()
+
+class TokenRequest(BaseModel):
+    ca: str
 
 @app.get("/")
 def root():
     return {"status": "online"}
+
+@app.post("/vamp/metadata")
+def metadata(req: TokenRequest):
+    return get_token_metadata(req.ca)
